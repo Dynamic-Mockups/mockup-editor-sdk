177a619ec0cb4dbec92c2b41853fcb9d01c603db
Milos Medic <milosmedicdev@gmail.com>
2024-12-03 12:02:54 +0100

    handdle bothe esm & iife

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,16 @@ import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "index.js",
-  output: {
-    file: "dist/index.js",
-    format: "iife",
-    name: "DynamicMockups",
-  },
+  output: [
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+    },
+    {
+      file: "dist/index.js",
+      format: "iife",
+      name: "DynamicMockups",
+    },
+  ],
   plugins: [resolve(), commonjs(), terser()],
 };
